3c0c24cddc76b4f352c82fb68e2207c611dd56db
Hemstreet <jonhemstreet@gmail.com>
2015-01-17 01:03:42 -0700

    v0.1.0

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,6 +2,7 @@ var referralSites = {
     'amazon.com': {
         'code':    '344342234',
         'pattern': 'tag'
+        //'callback' : amazon
     },
     'ebay.com':   {
         'code':    '47574748',
@@ -19,18 +20,28 @@ var domain = window.location.hostname.replace(/([a-zA-Z0-9]+.)/, "");
 for (var url in referralSites) {
 
     if (url == domain) {
-        var query = window.location.href.split('?')[1] || '';
-        var components = query.split('&')[1] || [];
 
         var site = referralSites[url];
 
-        var found = false;
+        var query = window.location.href.split('?')[1] || '';
 
-        for(var i = 0; i < components.length; i++) {
+        //site.callback();
+
+        var components = query.split('&') || [site.pattern + '=test'];
+
+        var found = false;
+        var redirect = true;
+
+        for (var i = 0; i < components.length; i++) {
             var component = components[i].split('=');
 
-            var key   = component[0];
+            var key = component[0];
             var value = component[1];
+
+            if (value == site.code) {
+                redirect = false;
+                break;
+            }
 
             if (key == site.pattern) {
                 found = true;
@@ -41,20 +52,27 @@ for (var url in referralSites) {
 
         }
 
-        if (!found) {
-            components.push(site.pattern + '=' + site.code);
+        if (redirect) {
+
+            if (!found) {
+                components.push(site.pattern + '=' + site.code);
+            }
+
+            query = components.join('&');
+
+            query = query.slice(1, query.length);
+
+            var path = window.location.href.split('?')[0] + '?' + query;
+
+            window.location.assign(path)
+
         }
 
-        query = components.join('&');
-
-        var path = window.location.href.split('?')[0] + '?' + query;
-
-        alert(path);
-
-        var referralString = site.code;
-        var referralPattern = site.pattern;
-        //window.location.assign("http://www.w3schools.com")
-        alert('matched domain');
     }
 
 }
+
+//function amazon() {
+//
+//}
+
