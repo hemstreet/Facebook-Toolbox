1a2a182e7f7f20fbc278af98ae45b93680dd288c
Naylor <jNaylor@o2oInteractive.com>
2015-01-17 00:23:50 -0700

    url query parsing

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,20 +1,55 @@
 var referralSites = {
     'amazon.com': {
         'code':    '344342234',
-        'pattern': 'tag='
+        'pattern': 'tag'
     },
-    'ebay.com':   {}
+    'ebay.com':   {
+        'code':    '47574748',
+        'pattern': 'refid'
+    }
 };
 
-var url = window.location.href;
+//var url = window.location.href;
 
 var domain = window.location.hostname.replace(/([a-zA-Z0-9]+.)/, "");
 
 //window.location.assign("http://www.w3schools.com")
+
+
 for (var url in referralSites) {
 
     if (url == domain) {
+        var query = window.location.href.split('?')[1] || '';
+        var components = query.split('&')[1] || [];
+
         var site = referralSites[url];
+
+        var found = false;
+
+        for(var i = 0; i < components.length; i++) {
+            var component = components[i].split('=');
+
+            var key   = component[0];
+            var value = component[1];
+
+            if (key == site.pattern) {
+                found = true;
+                value = site.code;
+
+            }
+
+
+        }
+
+        if (!found) {
+            components.push(site.pattern + '=' + site.code);
+        }
+
+        query = components.join('&');
+
+        var path = window.location.href.split('?')[0] + '?' + query;
+
+        alert(path);
 
         var referralString = site.code;
         var referralPattern = site.pattern;
